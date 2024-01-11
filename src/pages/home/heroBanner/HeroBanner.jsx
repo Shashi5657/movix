faec266a2afb9e098e514cf37bb0d98f99fcc667
Shashi5657 <sangepushashidhar@gmail.com>
2024-01-11 19:03:41 +0530

    movix project finished

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,22 +1,26 @@
-import React , { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {useSelector } from "react-redux"; 
+import { useSelector } from "react-redux";
 import "./style.scss";
+
 import useFetch from "../../../hooks/useFetch";
+
 import Img from "../../../components/lazyLoadImage/Img";
-import ContentWrapper from "../../../components/contentWrapper/contentWrapper";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
   const { data, loading } = useFetch("/movie/upcoming");
-  const {url} = useSelector((state) => state.home);
 
-  useEffect(()=> {
-    const bg =url.backdrop +  data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
     setBackground(bg);
-  },[data])
+  }, [data]);
 
   const searchQueryHandler = (event) => {
     if (event.key === "Enter" && query.length > 0) {
@@ -26,30 +30,30 @@ const HeroBanner = () => {
 
   return (
     <div className="heroBanner">
-      <div className="backdrop-img">
-        <Img src={background}/>
-      </div>
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
+
       <div className="opacity-layer"></div>
       <ContentWrapper>
-        <div className="wrapper">
         <div className="heroBannerContent">
           <span className="title">Welcome.</span>
           <span className="subTitle">
-            Millions of Movies, TV shows and people to discover. Explore now.
+            Millions of movies, TV shows and people to discover. Explore now.
           </span>
           <div className="searchInput">
             <input
               type="text"
-              placeholder="Search for a movie or TV show..."
+              placeholder="Search for a movie or tv show...."
               onChange={(e) => setQuery(e.target.value)}
               onKeyUp={searchQueryHandler}
             />
             <button>Search</button>
           </div>
         </div>
-      </div>
       </ContentWrapper>
-      
     </div>
   );
 };
